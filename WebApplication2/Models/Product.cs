ed9b72d4562f0043b66972bf143e223f6e9ab66a
Chibuezem Marvinrose <rozzeymarvin32@gmail.com>
2024-02-07 16:33:50 +0100

    Created the Models folder with the products class and created the models for the products

---
+++ WebApplication2/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿namespace WebApplication2.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = "";
+
+
+        public string Brand { get; set; } = "";
+
+        public string Category { get; set; } = "";
+
+        public decimal Price { get; set; }
+        public string Description { get; set; } = "";
+
+        public string ImageFileName { get; set; } = "";
+
+        public DateTime CreatedAt { get; set; }
+
+
+
+
+
+
+    }
+}
